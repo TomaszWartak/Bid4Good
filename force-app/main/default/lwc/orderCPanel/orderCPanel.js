efe218cd0a9d271be80d75bf2b9055a058bca788
tomaszwartak <tomasz.wartak@gmail.com>
2025-09-19 22:32:36 +0200

    Add due month picklist to LWC

--- force-app/main/default/lwc/orderCPanel/orderCPanel.js
+++ force-app/main/default/lwc/orderCPanel/orderCPanel.js
@@ -1,16 +1,17 @@
 import { LightningElement, track, wire } from "lwc";
 import getAccountsWithOrdersPicklistValues from "@salesforce/apex/OrderCController.getAccountsWithOrdersPicklistValues";
+import getPaymentDueDateMonthsPicklistValues from "@salesforce/apex/OrderCController.getPaymentDueDateMonthsPicklistValues";
 
 export default class OrderExplorer extends LightningElement {
   @track accountsPicklistValues = [];
   selectedAccountId;
-  isLoading = true;
+  isAccountPicklistLoading = true;
 
   @wire(getAccountsWithOrdersPicklistValues)
   wiredAccounts({ data, error }) {
     if (data) {
       this.accountsPicklistValues = data;
-      this.isLoading = false;
+      this.isAccountPicklistLoading = false;
       // TODO: console log - usunac pozniej
       console.log(
         "Accounts from Apex:",
@@ -26,20 +27,41 @@ export default class OrderExplorer extends LightningElement {
     this.selectedAccountId = event.detail.value;
   }
 
-  //   get hasAccounts() {
-  //     return (
-  //       this.accountsPicklistValues && this.accountsPicklistValues.length > 0
-  //     );
-  //   }
-
-  get isEmpty() {
+  get isAccountListEmpty() {
     return (
-      !this.isLoading &&
+      !this.isAccountPicklistLoading &&
       (!this.accountsPicklistValues || this.accountsPicklistValues.length === 0)
     );
   }
 
-  get isNotEmpty() {
-    return !this.isEmpty;
+  get isAccountListNotEmpty() {
+    return !this.isAccountListEmpty;
+  }
+
+  @track monthsDueDatePicklistValues = [];
+  selectedMonth;
+
+  // Months due date handling
+  @wire(getPaymentDueDateMonthsPicklistValues)
+  wiredMonthsDueDate({ data, error }) {
+    if (data) {
+      this.monthsDueDatePicklistValues = data;
+      // Set default value to the first option if available
+      if (data.length > 0) {
+        this.selectedMonth = data[0].value;
+      }
+      // TODO: console log - usunac pozniej
+      console.log(
+        "Months from Apex:",
+        JSON.stringify(this.monthsDueDatePicklistValues)
+      );
+    }
+    if (error) {
+      console.error(error);
+    }
+  }
+
+  handleMonthsDueDateChange(event) {
+    this.monthsDueDate = event.target.value;
   }
 }
